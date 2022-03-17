8cf093799836b14977c2ad2410b75c3dcb36f65b
vkamran7 <vkamran7@gmail.com>
2022-03-17 13:31:36 +0400

    adding User and Role classes

---
+++ src/main/java/com/project/petclinic/model/User.java
@@ -0,0 +1,68 @@
+package com.project.petclinic.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "enabled")
+    private Boolean enabled;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user", fetch = FetchType.EAGER)
+    private Set<Role> roles;
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getEnabled() {
+        return this.enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Set<Role> getRoles() {
+        return this.roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    @JsonIgnore
+    public void addRole(String roleName) {
+        if (this.roles == null) {
+            this.roles = new HashSet<>();
+        }
+
+        Role role = new Role();
+        role.setName(roleName);
+        this.roles.add(role);
+    }
+}
