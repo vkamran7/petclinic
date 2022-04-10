6b58822c024a50cb454d646d3a1f5e989e656c62
vkamran7 <vkamran7@gmail.com>
2022-04-10 11:37:09 +0400

    adding pet type

---
+++ src/main/java/com/project/petclinic/model/PetType.java
@@ -0,0 +1,9 @@
+package com.project.petclinic.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
+public class PetType extends NamedEntity {
+}
