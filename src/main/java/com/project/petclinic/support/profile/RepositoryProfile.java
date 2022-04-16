08cc136b41e79182edb73681054538d1091c2fa4
vkamran7 <vkamran7@gmail.com>
2022-04-17 03:01:48 +0400

    adding profiles for repository layer

---
+++ src/main/java/com/project/petclinic/support/profile/RepositoryProfile.java
@@ -0,0 +1,10 @@
+package com.project.petclinic.support.profile;
+
+public final class RepositoryProfile {
+
+    public static final String SPRING_DATA_JPA = "spring-data-jpa";
+
+    public static final String JPA = "jpa";
+
+    public static final String JDBC = "jdbc";
+}
