eeb85e64d1a62e18fc0a5e32fd79b8ed9a419704
vkamran7 <vkamran7@gmail.com>
2022-04-17 02:57:50 +0400

    adding SpringDataOwnerRepository

---
+++ src/main/java/com/project/petclinic/repository/springdatajpa/SpringDataOwnerRepository.java
@@ -0,0 +1,27 @@
+package com.project.petclinic.repository.springdatajpa;
+
+import com.project.petclinic.model.Owner;
+import com.project.petclinic.repository.OwnerRepository;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Collection;
+
+/**
+ * Spring Data JPA specialization of the {@link com.project.petclinic.repository.OwnerRepository} interface
+ *
+ * @author Kamran Valizada
+ */
+@Profile("spring-data-jpa")
+public interface SpringDataOwnerRepository extends OwnerRepository, Repository<Owner, Integer> {
+
+    @Override
+    @Query("SELECT DISTINCT owner FROM Owner owner LEFT JOIN FETCH owner.pets WHERE owner.lastName LIKE :lastName%")
+    Collection<Owner> findByLastName(@Param("lastName") String lastName);
+
+    @Override
+    @Query("SELECT owner FROM Owner owner LEFT JOIN FETCH owner.pets WHERE owner.id = :id")
+    Owner findById(@Param("id") int id);
+}
